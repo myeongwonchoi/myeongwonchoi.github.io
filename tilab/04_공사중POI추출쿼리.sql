952c16a426a34bfede912578156b0f16a5f5a8e6
Michael <105836115+myeongwonchoi@users.noreply.github.com>
2026-01-14 08:40:41 +0900

    Update 04_공사중POI추출쿼리.sql

--- tilab/04_공사중POI추출쿼리.sql
+++ tilab/04_공사중POI추출쿼리.sql
@@ -4,7 +4,7 @@
 	- 최초작성자: 최명원
 ****************************************/
 
-use POI_MAIN_PRACT
+USE POI_MAIN_PRACT
 
 SELECT DISTINCT a.POI_ID, a.UPDATE_DATE, c.CC_NAME, e.N_NAME AS 명칭, g.USER_NAME, h.CONSTRUCT_DATE, h.MEMO, a.err_code
 FROM PTM_COMMON a, PTC_POI_CLASS b,  PTC_CLASS c, PTD_NAME d, PTN_NAME e, PTN_TELEPHONE f, GTN_USER g, POI_MAIN_ETC..[22y_2150_INDEX_220401] x, PTN_CONSTRUCT h
@@ -14,3 +14,4 @@ AND LEFT(a.TILE_ID,4) = x.MAP_ID
 AND x.관할업체= '티아이랩'
 AND a.err_code NOT IN (4,260,516) -- 에러코드 4:ER, 260:ER+TE, 516:ER+UC
 ORDER BY h.CONSTRUCT_DATE
+
