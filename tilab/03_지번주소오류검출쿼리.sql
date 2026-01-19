171741ac080f39f22a1db07cbbb209ebf3d870a4
Michael <105836115+myeongwonchoi@users.noreply.github.com>
2026-01-19 21:10:22 +0900

    Update 03_지번주소오류검출쿼리.sql

--- tilab/03_지번주소오류검출쿼리.sql
+++ tilab/03_지번주소오류검출쿼리.sql
@@ -1,18 +1,38 @@
 /***************************************
-	- ��     ��: ���۾� �Է� ���� �ּҿ� ��ǥ ���� ���� �ּҰ� ���� ������ ��� ����
-	- �����ۼ���: 2024-04-08
-	- �����ۼ���: �ָ���
-	- ����������: 
-	- �����ۼ���: 
+	- 제     목: 수작업 입력 지번 주소와 좌표 기준 지번 주소가 서로 상이한 대상 검출
+	- 최초작성일: 2024-04-08
+	- 최초작성자: 최명원
+	- 최종수정일: 
+	- 최종작성자: 
 ****************************************/
 
-USE POI_MAIN_PRACT
+USE POI_MAIN_PRACT;
+GO
 
-SELECT a.poi_id, a.pname, a.insert_date, a.insert_user, a.update_date, a.update_user, b.user_name, c.address, c.jibun_m , c.jibun_s , e.lnbr_mnnm , e.lnbr_slno
-FROM PTM_COMMON a, GTN_USER b, PTN_ADDR c, CTX_PTC_ROAD_ADDR_BULD_MATCHING d, PTN_NEAD_BULD e
-WHERE a.update_user = b.user_seq AND a.poi_id = c.poi_id AND a.poi_id = d.poi_id AND d.sig_cd = e.sig_cd AND d.road_id = e.road_id AND d.buld_id = e.buld_id
-AND b.user_seq = 3286
-AND (c.jibun_m <> e.lnbr_mnnm OR c.jibun_s <> e.lnbr_slno)
-AND ERR_CODE NOT IN (4,260)
-ORDER BY a.update_date DESC
-
+SELECT 
+      A.POI_ID
+    , A.PNAME
+    , A.INSERT_DATE
+    , A.INSERT_USER
+    , A.UPDATE_DATE
+    , A.UPDATE_USER
+    , B.USER_NAME
+    , C.ADDRESS AS [수작업_주소]
+    , C.JIBUN_M AS [수작업_본번], C.JIBUN_S AS [수작업_부번]
+    , E.LNBR_MNNM AS [매칭_본번], E.LNBR_SLNO AS [매칭_부번]
+FROM PTM_COMMON AS A WITH (NOLOCK)
+INNER JOIN GTN_USER AS B WITH (NOLOCK) 
+    ON A.UPDATE_USER = B.USER_SEQ
+INNER JOIN PTN_ADDR AS C WITH (NOLOCK) 
+    ON A.POI_ID = C.POI_ID
+INNER JOIN CTX_PTC_ROAD_ADDR_BULD_MATCHING AS D WITH (NOLOCK) 
+    ON A.POI_ID = D.POI_ID
+INNER JOIN PTN_NEAD_BULD AS E WITH (NOLOCK) 
+    ON D.SIG_CD = E.SIG_CD 
+   AND D.ROAD_ID = E.ROAD_ID 
+   AND D.BULD_ID = E.BULD_ID
+WHERE B.USER_SEQ = 3286
+  AND A.ERR_CODE NOT IN (4, 260)
+  /* 지번 불일치 조건: 본번 혹은 부번이 하나라도 다르면 검출 */
+  AND (C.JIBUN_M <> E.LNBR_MNNM OR C.JIBUN_S <> E.LNBR_SLNO)
+ORDER BY A.UPDATE_DATE DESC;
