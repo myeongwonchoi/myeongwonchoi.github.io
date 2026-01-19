4d61a4249c16dbb9e0995da49687a1ffa6d2d105
Michael <105836115+myeongwonchoi@users.noreply.github.com>
2026-01-19 21:44:24 +0900

    Update 25_현장평가 리스트업.sql

--- tilab/25_현장평가 리스트업.sql
+++ tilab/25_현장평가 리스트업.sql
@@ -6,14 +6,11 @@
 	- 최종작성자: 
 ************************************************************************/
 
-/*
- 1. CORE 대상 추출해서 엑셀 파일에 기입하기
-*/
+/*  1. CORE 대상 추출해서 엑셀 파일에 기입하기 */
 USE POI_MAIN_PRACT
 
-SELECT DISTINCT POI_ID, FNAME, CNAME, CP_ID,
-ADDR +' '+ CAST(PRIMARY_BUN AS varchar(10)) + '-' + CAST(SECONDARY_BUN AS varchar(10)) AS 구주소,
-TELE_A + '-' + TELE_B +  '-' +TELE_C AS 전화번호
+SELECT DISTINCT POI_ID, FNAME, CNAME, CP_ID, ADDR +' '+ CONCAT(ADDR, ' ', PRIMARY_BUN, '-', SECONDARY_BUN) AS [구주소],
+CONCAT_WS('-', NULLIF(TELE_A, ''), NULLIF(TELE_B, ''), NULLIF(TELE_C, '')) AS [전화번호]
 FROM vPOI_I_COMMON_ALL_GRS
 -- 가고자 하는 동 입력
 WHERE ADDR LIKE '%'
@@ -24,12 +21,9 @@ AND SECONDARY_BUN IN ()
 ORDER BY FNAME, CNAME, 구주소
 
 
-/*
- 2. CORE가 아닌 CP 대상 추출해서 엑셀 파일에 기입하기
- */
-SELECT POI_ID, FNAME, CNAME, CP_ID,
-ADDR +' '+ CAST(PRIMARY_BUN AS varchar(10)) + '-' + CAST(SECONDARY_BUN AS varchar(10)) AS 구주소,
-TELE_A + '-' + TELE_B +  '-' +TELE_C AS 전화번호
+/*  2. CORE가 아닌 CP 대상 추출해서 엑셀 파일에 기입하기 */
+SELECT  POI_ID, FNAME, CNAME, CP_ID, ADDR +' '+ CONCAT(ADDR, ' ', PRIMARY_BUN, '-', SECONDARY_BUN) AS [구주소],
+CONCAT_WS('-', NULLIF(TELE_A, ''), NULLIF(TELE_B, ''), NULLIF(TELE_C, '')) AS [전화번호]
 FROM CP_MAIN_CONV_OBN_BAK..MID_CP_COMMON_9월2차_20250918
 -- 가고자 하는 동 입력
 WHERE ADDR LIKE '%'
@@ -55,3 +49,4 @@ SELECT POI_ID, ERR_CODE
 FROM PTM_COMMON
 WHERE POI_ID IN ( )
 AND ERR_CODE IN (2, 34, 6, 262, 66, 98, 354, 258, 770, 514, 1, 4, 68, 100, 324, 356, 836, 580, 612, 260, 292, 772, 516)
+
